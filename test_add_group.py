790bd92eedc9acb0f5faa158739a1f189610c986
gipuk <cubek@o2.pl>
2017-02-20 20:53:55 +0100

    wyodrebniona klasa do pracy z grupami

--- test_add_group.py
+++ test_add_group.py
@@ -15,11 +15,11 @@ def app(request):
 
 def test_add_group(app):
     app.sesion.enter_login(username="admin", password="secret")
-    app.create_group(Group(name="test python", header="test", footer="testtest"))
+    app.group.create(Group(name="test python", header="test", footer="testtest"))
     app.sesion.logout()
 
 
 def test_add_empty_group(app):
     app.sesion.enter_login(username="admin", password="secret")
-    app.create_group(Group (name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.sesion.logout()
